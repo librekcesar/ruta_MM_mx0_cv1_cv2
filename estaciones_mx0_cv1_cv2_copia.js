a924c694db26adabed1c4a442258fe9832e4e062
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-15 17:32:06 -0600

    Add files via upload

---
+++ estaciones_mx0_cv1_cv2_copia.js
@@ -0,0 +1,10 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_cv1_cv2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "18.851778543817353, -99.1973140215011", "description": "CV2", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -99.1971064, 18.8517632, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Priv. Rio 32b, El Arroyo, 62553 Jiutepec, Mor., México", "description": "CV1", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.1782676, 18.8885855, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "19.64220097106514, -99.14250999381147", "description": "MX0", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.1422607, 19.6422227, 0.0 ] } }
+]
+}
